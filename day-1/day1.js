0732ab74c6f9a88735b103e951efbc3dd5d688e4
Musa <79163385+musayuksel@users.noreply.github.com>
2022-12-01 13:57:53 +0000

    Add day-1 data

---
+++ day-1/day1.js
@@ -0,0 +1,5 @@
+//get the data from the file
+// convert it to 2D array ([[1000,2000,3000],[4000]])
+// loop through the array and find totals [[1000,2000,3000],[4000]] => [6000,4000]
+// find the max of the totals
+
