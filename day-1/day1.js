913b97dd5280387633be531dc3cf86049dfd547d
Musa <79163385+musayuksel@users.noreply.github.com>
2022-12-01 14:00:49 +0000

    Add readData function

--- day-1/day1.js
+++ day-1/day1.js
@@ -3,3 +3,10 @@
 // loop through the array and find totals [[1000,2000,3000],[4000]] => [6000,4000]
 // find the max of the totals
 
+function readData() {
+  const fs = require('fs');
+  const buffer = fs.readFileSync('data.txt', 'utf8');
+  const dataString = buffer.toString();
+  console.log({ dataString: dataString.split('\n') });
+}
+readData();
