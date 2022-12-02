3b15b0ae18efc7fa0adac49f3192d46ba6f1d83b
Musa <79163385+musayuksel@users.noreply.github.com>
2022-12-02 14:16:57 +0000

    Add PART-2 findTotalScoreWithNewMoves

--- day-2/findTotalScore.js
+++ day-2/findTotalScore.js
@@ -24,4 +24,32 @@ function findTotalScore() {
     0
   );
 }
-console.log(readData(), {findTotalScore:findTotalScore()});//[ 'A Y', 'B X', 'C Z' ] { findTotalScore: 15 }
+console.log(readData(), {
+  findTotalScore: findTotalScore(),
+}); //[ 'A Y', 'B X', 'C Z' ] { findTotalScore: 15 }
+
+//------------ Part 2
+
+function findTotalScoreWithNewMoves() {
+  const newMoves = {
+    'A X': 'A Z', // X will lose so change it to Z
+    'A Y': 'A X',
+    'A Z': 'A Y',
+    'B X': 'B X',
+    'B Y': 'B Y',
+    'B Z': 'B Z',
+    'C X': 'C Y',
+    'C Y': 'C Z',
+    'C Z': 'C X',
+  };
+  const data = readData();
+  const movesAfterNewStrategy = data.map((move) => newMoves[move]);
+
+  return movesAfterNewStrategy.reduce(
+    (sum, roundMove) => sum + possibleCombinations[roundMove],
+    0
+  );
+}
+console.log({
+  findTotalScoreWithNewMoves: findTotalScoreWithNewMoves(),
+}); //[ 'A Y', 'B X', 'C Z' ] { findTotalScore: 12 }
