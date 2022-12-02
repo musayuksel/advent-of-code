101727d00b81a58fa08433f22e998ff0405faf42
Musa <79163385+musayuksel@users.noreply.github.com>
2022-12-02 13:47:20 +0000

    possibleCombinations

--- day-2/findTotalScore.js
+++ day-2/findTotalScore.js
@@ -1,7 +1,18 @@
 function readData() {
-    const fs = require('fs');
-    const buffer = fs.readFileSync('data.txt', 'utf8');
-    const dataString = buffer.toString();
-    return dataString.split('\n');
-  }
-console.log(readData());  
+  const fs = require('fs');
+  const buffer = fs.readFileSync('data.txt', 'utf8');
+  const dataString = buffer.toString();
+  return dataString.split('\n');
+}
+console.log(readData().length);
+const possibleCombinations = {
+  'A X': 4,
+  'A Y': 8,
+  'A Z': 3,
+  'B X': 1,
+  'B Y': 5,
+  'B Z': 9,
+  'C X': 7,
+  'C Y': 2,
+  'C Z': 6,
+};
