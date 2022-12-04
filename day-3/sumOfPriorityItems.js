308d3edbd334ed835ef08f36b9dd0b5cce8bf589
Musa <79163385+musayuksel@users.noreply.github.com>
2022-12-04 10:09:15 +0000

    Fix:  find total value for each line separately

--- day-3/sumOfPriorityItems.js
+++ day-3/sumOfPriorityItems.js
@@ -11,11 +11,43 @@ function readData() {
 }
 
 function sliceText(textArray) {
-  //{left:'vJrwpWtwJgWr', right:'JgWr'}
+  //[{left:'vJrwpWtwJgWr', right:'JgWr'}]
   return textArray.map((text) => ({
     left: text.slice(0, text.length / 2),
     right: text.slice(text.length / 2),
   }));
 }
 
-console.log(sliceText(readData()));
+function findTheCommonLettersAndValues(slicedTextArray) {
+  const allLetters = '_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  return slicedTextArray.map((slicedText) => {
+    const commonLettersAndValues = {};
+    slicedText.left.split('').forEach((letter) => {
+      if (
+        slicedText.right.includes(letter) &&
+        !commonLettersAndValues[letter]
+      ) {
+        commonLettersAndValues[letter] = allLetters.indexOf(letter);
+      }
+    });
+    return commonLettersAndValues;
+  });
+}
+
+function findTotalValueOfOneObject(object) {
+    //if one object includes more than one common char {A: 1, B: 2} => 3
+  return Object.values(object).reduce((sum, value) => sum + value, 0);
+}
+
+function sumOfPriorityItems() {
+  const data = readData();
+  const slicedTexts = sliceText(data);
+  const commonLettersAndValues = findTheCommonLettersAndValues(slicedTexts);
+  console.log({ commonLettersAndValues });
+
+  return commonLettersAndValues.reduce((sum, object) => {
+    return sum + findTotalValueOfOneObject(object);
+  }, 0);
+}
+
+console.log(sumOfPriorityItems());
