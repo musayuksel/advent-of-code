43a0078a88dabf49d497e4bcf96fe817cd2dcb82
Musa <79163385+musayuksel@users.noreply.github.com>
2022-12-05 13:25:34 +0000

    Add sliceDataIntoStackAndMoves

--- day-5/findOnTopOfEachStack.js
+++ day-5/findOnTopOfEachStack.js
@@ -5,4 +5,23 @@ function readData() {
   return dataString.split('\n');
 }
 
-console.log(readData());
+function sliceDataIntoStackAndMoves(data) {
+  //  stack=[
+  //   '    [D]    ',
+  //   '[N] [C]    ',
+  //   '[Z] [M] [P]',
+  //   ' 1   2   3 ']
+  //  moves=[
+  //   'move 1 from 2 to 1',
+  //   'move 3 from 1 to 3',
+  //   'move 2 from 2 to 1',
+  //   'move 1 from 1 to 2'
+  // ]
+  const indexOfEmpty = data.findIndex((text) => text === '');
+  return ([stack, moves] = [
+    data.slice(0, indexOfEmpty),
+    data.slice(indexOfEmpty + 1),
+  ]);
+}
+
+console.log(sliceDataIntoStackAndMoves(readData()));
