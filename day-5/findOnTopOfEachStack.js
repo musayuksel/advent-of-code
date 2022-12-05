50d32c87386a49341c906e33d3c7237968c82b0c
Musa <79163385+musayuksel@users.noreply.github.com>
2022-12-05 13:19:28 +0000

    Add example data and readData function

---
+++ day-5/findOnTopOfEachStack.js
@@ -0,0 +1,8 @@
+function readData() {
+  const fs = require('fs');
+  const buffer = fs.readFileSync('data.txt', 'utf8');
+  const dataString = buffer.toString();
+  return dataString.split('\n');
+}
+
+console.log(readData());
