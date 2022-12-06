6d0ae1a83a39377a52099bf5f6136b1066e8b807
Musa <79163385+musayuksel@users.noreply.github.com>
2022-12-06 10:14:25 +0000

    Read chars 4 by 4

--- day-6/findStartOfPacketMarker.js
+++ day-6/findStartOfPacketMarker.js
@@ -5,8 +5,16 @@ function readData() {
   return dataString;
 }
 
+function findFirstUniqueMarkerIndex(data) {
+  for (let i = 0; i < data.length; i++) {
+    const fourCharacters = data.slice(i, i + 4);
+    console.log({ fourCharacters });
+  }
+}
+
 function findStartOfPacketMarker() {
   const data = readData();
   console.log({ data });
+  findFirstUniqueMarkerIndex(data);
 }
 findStartOfPacketMarker();
