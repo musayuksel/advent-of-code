adc0f717fb4efe286250ccd0d39d4c4c5d2093e2
Musa <79163385+musayuksel@users.noreply.github.com>
2022-12-08 13:27:29 +0000

    convertDataTo2DGrid with indexes

--- day-8/findTreesVisibleOutside.js
+++ day-8/findTreesVisibleOutside.js
@@ -5,8 +5,21 @@ function readData() {
   return dataString.split('\n');
 }
 
+function convertDataTo2DGrid(data) {
+  //['30373'] => [[{hight:3, rowIndex:0, columnIndex:0}], [{hight:0, rowIndex:0, columnIndex:1}], [{hight:3, rowIndex:0, columnIndex:2}], [{hight:7, rowIndex:0, columnIndex:3}], [{hight:3, rowIndex:0, columnIndex:4}]]
+  return data.map((row, rowIndex) => {
+    return row.split('').map((column, columnIndex) => ({
+      hight: parseInt(column),
+      rowIndex,
+      columnIndex,
+    }));
+  });
+}
+
 function findTreesVisibleOutside() {
-  const grid = readData();
-  console.log({ grid });
+  const data = readData();
+  const grid = convertDataTo2DGrid(data);
+  console.log({ data });
+  console.dir(grid, { depth: null });
 }
 findTreesVisibleOutside();
