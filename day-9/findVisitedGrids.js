1334767387749d11d274f804bf6e1d3fb2dedbb6
Musa <79163385+musayuksel@users.noreply.github.com>
2022-12-09 21:41:55 +0000

    Add algorithm as a comment

--- day-9/findVisitedGrids.js
+++ day-9/findVisitedGrids.js
@@ -28,6 +28,15 @@ const moveUpLeft = (position) => moveOneUp(moveOneLeft(position));
 const moveDownRight = (position) => moveOneDown(moveOneRight(position));
 const moveDownLeft = (position) => moveOneDown(moveOneLeft(position));
 
+//Current Tail: [1,1] & current Head: [1,3] => Diff: [0,2] Right
+// Diff [0,-2] Left
+// Diff [-2,0] Up
+// Diff [2,0] Down
+// Diff [-2,1] || [-1,2] UpRight
+// Diff [-2,-1] || [-1,-2] UpLeft
+// Diff [2,1] || [1,2] DownRight
+// Diff [2,-1] || [1,-2] DownLeft
+
 function findVisitedGrids() {
   const movesString = readData();
   const moves = convertDataToMoves(movesString);
