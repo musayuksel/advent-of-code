c815a07735dd8d8748047654d9296d14dab9ca22
Musa <79163385+musayuksel@users.noreply.github.com>
2022-12-12 21:27:53 +0000

    Find start and end points

--- day-12/findShortestWay.js
+++ day-12/findShortestWay.js
@@ -9,5 +9,24 @@ function findShortestWay() {
   const data = readData();
   const routeArray = data.map((route) => route.split(''));
   console.dir(routeArray, { depth: null });
+  // find the starting point 'S'
+  //find the end point 'E'
+  // find the shortest way to the end point from the starting point
+  // only move in the four cardinal directions
+  // only move at most one letter later in the alphabet
+  // (e.g. from 'a' to 'b', but not from 'a' to 'c')
+
+  const startingPointRow = routeArray.findIndex((row) => row.includes('S'));
+  const startingPointColumn = routeArray[startingPointRow].indexOf('S');
+
+  const endPointRow = routeArray.findIndex((row) => row.includes('E'));
+  const endPointColumn = routeArray[endPointRow].indexOf('E');
+
+  console.log(
+    startingPointRow,
+    startingPointColumn,
+    endPointRow,
+    endPointColumn
+  );
 }
 findShortestWay();
