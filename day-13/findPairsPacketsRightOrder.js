0f8621647afa5411ebab1a3a8ec95b64e452bff7
Musa <79163385+musayuksel@users.noreply.github.com>
2022-12-16 23:48:37 +0000

    Try recursive

--- day-13/findPairsPacketsRightOrder.js
+++ day-13/findPairsPacketsRightOrder.js
@@ -8,86 +8,65 @@ function readData() {
 function findPairsArrays(data) {
   return data.map((item) => {
     const [left, right] = item.split('\n');
-    // return [eval(left), eval(right)];
-    return [left, right];
+    return [eval(left), eval(right)];
+    // return [left, right];
   });
 }
 
-function convertStringsToComparableArray(stringArray) {
-  //[[[123]] =>[Infinity,Infinity,Infinity,123]
-  // [1,[15],3,1,1] => [Infinity, 1, Infinity, 15, 3, 1, 1 ]
-  const newArray = [];
-  const numbers = '1234567890';
-  const stringChars = stringArray.split('');
-  let currentIndex = 0;
-  while (currentIndex < stringChars.length) {
-    const currentChar = stringChars[currentIndex];
-    if (currentChar === '[') {
-      newArray.push(Infinity);
-    } else if (numbers.includes(currentChar)) {
-      let nextSliceIndex = stringArray.indexOf(',', currentIndex);
-      const nextBreakIndex = stringArray.indexOf(']', currentIndex);
-      if (nextSliceIndex === -1) {
-        nextSliceIndex = nextBreakIndex;
-      } else if (nextSliceIndex > nextBreakIndex) {
-        nextSliceIndex = nextBreakIndex;
-      }
-      const nextNum = stringArray.slice(currentIndex, nextSliceIndex);
-      newArray.push(+nextNum);
 
-      currentIndex = nextSliceIndex;
-    }
-    currentIndex++;
+
+let result =false;
+function isPairsRightOrder(leftArray, rightArray){
+  // const [leftArray, rightArray] = pair;
+  console.log('>>>>>>>>>>',{leftArray,rightArray})
+ for (let i = 0; i < leftArray.length; i++) {
+    const leftNumber = leftArray[i];
+    const rightNumber = rightArray[i];
+  
+  if(typeof leftNumber === 'object' && typeof rightNumber === 'object') {
+    // if(!isPairsRightOrder(leftNumber,rightNumber)) return false;
+    // continue;
+    console.log('both are objects')
+     isPairsRightOrder(leftNumber,rightNumber);
   }
-  return newArray;
-}
 
-function isPairsRightOrder(pair) {
-  const [leftArray, rightArray] = pair;
-  console.log({ leftArray, rightArray });
-
-  while (true) {
-    const leftNumber = leftArray.shift();
-    const rightNumber = rightArray.shift();
-
-    if (leftNumber === undefined) {
-      console.log('LEFT side ran out of items', { leftNumber, rightNumber });
-      if (leftNumber > rightNumber) return false;
-      return true;
-    }
-    if (rightNumber === undefined) {
-      console.log('Right side ran out of items:', { leftNumber, rightNumber });
-      return false;
-    }
-    if (leftNumber === Infinity && rightNumber === Infinity) continue;
-    if (leftNumber === Infinity && rightNumber !== Infinity) {
-      //push right number back
-      rightArray.unshift(rightNumber);
-      continue;
-    }
-    if (leftNumber !== Infinity && rightNumber === Infinity) {
-      //push left number back
-      leftArray.unshift(leftNumber);
-      continue;
-    }
-    if (leftNumber !== Infinity && rightNumber !== Infinity) {
-      if (leftNumber > rightNumber) return false;
-      if (leftNumber === rightNumber) continue;
-      return true;
-    }
+  if(typeof leftNumber === 'object' && typeof rightNumber !== 'object') {
+    console.log('left is object')
+    isPairsRightOrder(leftNumber,rightArray);
   }
-}
 
+  if(typeof leftNumber !== 'object' && typeof rightNumber === 'object') {
+    console.log('right is object')
+    isPairsRightOrder(leftArray,rightNumber);
+  }
+  if(typeof rightNumber === 'undefined') {result=true}
+
+  if(leftNumber < rightNumber) {result=true}
+  if(leftNumber > rightNumber) return false;
+  if(leftNumber === rightNumber) continue;
+ }
+    
+}
+// console.log(isPairsRightOrder([[1],[2,3,4]],[[1],4]))
+console.log(isPairsRightOrder([[1],[2,3,4]], [[1],4]))
+console.log({result})
 function findPairsPacketsRightOrder() {
   const data = readData();
   const pairs = findPairsArrays(data);
+    console.dir(pairs, { depth: null });
 
-  const pairsArray = pairs.map(([left, right]) => [
-    convertStringsToComparableArray(left),
-    convertStringsToComparableArray(right),
-  ]);
+  // const pairsArray = pairs.map(([left, right]) => [
+  //   convertStringsToComparableArray(left),
+  //   convertStringsToComparableArray(right),
+  // ]);
 
-  console.log(pairsArray.map(isPairsRightOrder));
-  //   console.dir(pairsArray, { depth: null });
+  // const rightOrderPairsIndexNumbers = [];
+
+  // pairsArray.forEach((pair, index) => {
+  //   isPairsRightOrder(pair) && rightOrderPairsIndexNumbers.push(index + 1);
+  // });
+
+  // const sumOfIndexNumbers = rightOrderPairsIndexNumbers.reduce((acc, item) => acc + item,0);
+  // console.log({rightOrderPairsIndexNumbers,sumOfIndexNumbers});
 }
-findPairsPacketsRightOrder();
+// findPairsPacketsRightOrder();
