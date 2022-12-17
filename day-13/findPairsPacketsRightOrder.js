70d73f93e85f89be7dcf6496d625daa9d268c8ee
Musa <79163385+musayuksel@users.noreply.github.com>
2022-12-17 00:40:50 +0000

    Working with example data not TEST data

--- day-13/findPairsPacketsRightOrder.js
+++ day-13/findPairsPacketsRightOrder.js
@@ -13,60 +13,65 @@ function findPairsArrays(data) {
   });
 }
 
-
-
-let result =false;
-function isPairsRightOrder(leftArray, rightArray){
+let result = false;
+function isPairsRightOrder(leftArray, rightArray) {
   // const [leftArray, rightArray] = pair;
-  console.log('>>>>>>>>>>',{leftArray,rightArray})
- for (let i = 0; i < leftArray.length; i++) {
+  // console.log('START>>>>>>>>>>',{leftArray,rightArray})
+  if (leftArray.length === 0 && rightArray.length > 0) {
+    result = true;
+  }
+  // if(leftArray.length ===0 && rightArray.length ===0) result= true;
+  for (let i = 0; i < leftArray.length; i++) {
     const leftNumber = leftArray[i];
     const rightNumber = rightArray[i];
-  
-  if(typeof leftNumber === 'object' && typeof rightNumber === 'object') {
-    // if(!isPairsRightOrder(leftNumber,rightNumber)) return false;
-    // continue;
-    console.log('both are objects')
-     isPairsRightOrder(leftNumber,rightNumber);
-  }
+    // console.log('COMPARE>>>>>>>>>>',{leftNumber,rightNumber})
+    if (typeof rightNumber === undefined) {
+      result = false;
+      return false;
+    }
+    if (typeof leftNumber === 'object' && typeof rightNumber === 'object') {
+      // console.log('both are objects');
+      isPairsRightOrder(leftNumber, rightNumber);
+    }
 
-  if(typeof leftNumber === 'object' && typeof rightNumber !== 'object') {
-    console.log('left is object')
-    isPairsRightOrder(leftNumber,rightArray);
-  }
+    if (typeof leftNumber === 'object' && typeof rightNumber !== 'object') {
+      // console.log('left is object');
+      isPairsRightOrder(leftNumber, rightArray.slice(i));
+    }
 
-  if(typeof leftNumber !== 'object' && typeof rightNumber === 'object') {
-    console.log('right is object')
-    isPairsRightOrder(leftArray,rightNumber);
-  }
-  if(typeof rightNumber === 'undefined') {result=true}
+    if (typeof leftNumber !== 'object' && typeof rightNumber === 'object') {
+      // console.log('right is object');
+      isPairsRightOrder(leftArray.slice(i), rightNumber);
+    }
 
-  if(leftNumber < rightNumber) {result=true}
-  if(leftNumber > rightNumber) return false;
-  if(leftNumber === rightNumber) continue;
- }
-    
+    // console.log("first")
+    if (leftNumber < rightNumber) {
+      result = true;
+      return true;
+    }
+    if (leftNumber > rightNumber) return false;
+    if (i === leftArray.length - 1 && rightArray[i + 1] !== undefined) {
+      // console.log(">>>>>",rightArray[i+1])
+      result = true;
+      return true;
+    }
+    if (leftNumber === rightNumber) continue;
+  }
 }
-// console.log(isPairsRightOrder([[1],[2,3,4]],[[1],4]))
-console.log(isPairsRightOrder([[1],[2,3,4]], [[1],4]))
-console.log({result})
+
+findPairsPacketsRightOrder();
 function findPairsPacketsRightOrder() {
   const data = readData();
   const pairs = findPairsArrays(data);
-    console.dir(pairs, { depth: null });
-
-  // const pairsArray = pairs.map(([left, right]) => [
-  //   convertStringsToComparableArray(left),
-  //   convertStringsToComparableArray(right),
-  // ]);
-
-  // const rightOrderPairsIndexNumbers = [];
-
-  // pairsArray.forEach((pair, index) => {
-  //   isPairsRightOrder(pair) && rightOrderPairsIndexNumbers.push(index + 1);
-  // });
-
-  // const sumOfIndexNumbers = rightOrderPairsIndexNumbers.reduce((acc, item) => acc + item,0);
-  // console.log({rightOrderPairsIndexNumbers,sumOfIndexNumbers});
+  // console.dir(pairs, { depth: null });
+  let total = 0;
+  pairs.forEach((pair, index) => {
+    const [leftArray, rightArray] = pair;
+    isPairsRightOrder(leftArray, rightArray);
+    console.log({ leftArray, rightArray, result });
+    if (result) total += index + 1;
+    result = false;
+  });
+  console.log({ total });
 }
 // findPairsPacketsRightOrder();
